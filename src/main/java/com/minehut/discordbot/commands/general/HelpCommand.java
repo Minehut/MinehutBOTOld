409aa942634029781be4be8aef5be10fdedfa3c8
MatrixTunnel <MatrixTunnel101@gmail.com>
2017-04-24 00:04:52 -0700

    Make the help command great

--- src/main/java/com/minehut/discordbot/commands/general/HelpCommand.java
+++ src/main/java/com/minehut/discordbot/commands/general/HelpCommand.java
@@ -3,6 +3,8 @@ package com.minehut.discordbot.commands.general;
 import com.minehut.discordbot.commands.Command;
 import com.minehut.discordbot.commands.CommandType;
 import com.minehut.discordbot.util.Chat;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
@@ -27,7 +29,17 @@ public class HelpCommand implements Command {
     public void onCommand(Guild guild, TextChannel channel, Member sender, Message message, String[] args) {
         Chat.removeMessage(message);
 
-        Chat.sendMessage("What do you need help with? I'm still making this command so you might have to wait a bit... Ask a staff member instead", channel, 15);
+        Chat.sendMessage(new MessageBuilder().append(sender.getAsMention()).setEmbed(Chat.getEmbed().setColor(Chat.CUSTOM_DARK_GREEN)
+                .addField("Music",
+                        "`" + Command.getPrefix() + "play <term>` Replace \"term\" with a YouTube/SoundCloud url or a search query to play music\n" +
+                        "`" + Command.getPrefix() + "queue` Lists the current music playlist of queued messages\n" +
+                        "`" + Command.getPrefix() + "skip` Casts your vote to skip the song that is currently playing\n" +
+                        "`" + Command.getPrefix() + "random <playlist>` Plays a random song from the specified category", false)
+                .addField("Minehut",
+                        "`" + Command.getPrefix() + "status <network|bot>` See Minehut's network or bot status\n" +
+                        "`" + Command.getPrefix() + "user <username>` Shows username changes and Minehut stats\n" +
+                        "`" + Command.getPrefix() + "server <name>` Shows about for the player server", false).build()).build(), channel, 20);
+        )
     }
 
     @Override
